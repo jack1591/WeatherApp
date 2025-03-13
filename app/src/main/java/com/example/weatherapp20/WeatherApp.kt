46033dc0b6616fccf425afeb35f442a603c5fae3
Artem <jack.tag@mail.ru>
2025-03-13 21:39:32 +0300

    mvi architecture application

---
+++ app/src/main/java/com/example/weatherapp20/WeatherApp.kt
@@ -0,0 +1,9 @@
+package com.example.weatherapp20
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApp: Application() {
+
+}
---
+++ app/src/main/java/com/example/weatherapp20/presentation/WeatherState.kt
@@ -0,0 +1,9 @@
+package com.example.weatherapp20.presentation
+
+import com.example.weatherapp20.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
